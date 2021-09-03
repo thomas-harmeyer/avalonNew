7afae11f5957d1bd1887b6bad23efacd350cbfd8
Thomas Harmeyer <thomasmharmeyer@gmail.com>
2021-09-03 11:03:57 -0400

    removed get and post requests

--- backend/src/app-controller.ts
+++ backend/src/app-controller.ts
@@ -11,22 +11,3 @@ app.use(
     extended: true,
   })
 );
-
-app.get("/settings", (req: any, res: any) => {
-  const game = findGame(req.query.ope);
-  res.send(game);
-});
-
-app.post("/settings", (req: any, res: any) => {
-  const gameSettings: Game = req.body.game;
-  const game = findGame(req.body.game.ope);
-  game.roles = gameSettings.roles;
-  game.totalPlayers = gameSettings.totalPlayers;
-
-  emitGame(game);
-  res.send(game);
-});
-
-app.listen(5000, () => {
-  console.log("settings server listening on /:5000");
-});
